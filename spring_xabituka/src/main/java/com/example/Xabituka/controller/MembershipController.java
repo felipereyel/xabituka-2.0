446eb21b712a8bd50fddda963a4afdf623eb0bf1
felipereyel <felipe.reyel@gmail.com>
2020-05-10 20:15:08 -0300

    add add user to group

---
+++ spring_xabituka/src/main/java/com/example/Xabituka/controller/MembershipController.java
@@ -0,0 +1,87 @@
+package com.example.Xabituka.controller;
+
+import com.example.Xabituka.model.Groups;
+import com.example.Xabituka.model.User_Group;
+import com.example.Xabituka.model.Users;
+import com.example.Xabituka.repository.GroupsRepository;
+import com.example.Xabituka.repository.MessagesRepository;
+import com.example.Xabituka.repository.UserGroupRepository;
+import com.example.Xabituka.repository.UsersRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping({"/membership"})
+public class MembershipController {
+
+    private final UserGroupRepository repository;
+    private final GroupsRepository groupsRepository;
+    private final MessagesRepository messagesRepository;
+    private final UsersRepository usersRepository;
+
+    public MembershipController(
+            UserGroupRepository repository,
+            GroupsRepository groupsRepository,
+            MessagesRepository messagesRepository,
+            UsersRepository usersRepository
+    ) {
+        this.repository = repository;
+        this.groupsRepository = groupsRepository;
+        this.messagesRepository = messagesRepository;
+        this.usersRepository = usersRepository;
+    }
+
+    @GetMapping(path = {"/{groupId}/add/{userId}"})
+    public LinkedHashMap add(
+            @RequestHeader("Authorization") String token,
+            @PathVariable Long groupId,
+            @PathVariable Long userId,
+            @RequestParam(defaultValue = "false") boolean admin
+    ) {
+        Map res = new LinkedHashMap();
+
+        Optional<Users> optSelfUser = usersRepository.findByToken(token);
+        if(optSelfUser.isEmpty()){
+            res.put("success", false);
+            res.put("reason", "Invalid token");
+            return (LinkedHashMap) res;
+        }
+        Users selfUser = optSelfUser.get();
+
+        Optional<User_Group> optMembership = repository.findByUserIdAndGroupId(selfUser.getId(), groupId);
+        if(optMembership.isEmpty()){
+            res.put("success", false);
+            res.put("reason", "Invalid membership");
+            return (LinkedHashMap) res;
+        }
+        User_Group selfMembership = optMembership.get();
+
+        if((selfMembership.getExitedAt() != null) || (!selfMembership.isAdmin())){
+            res.put("success", false);
+            res.put("reason", "Invalid membership");
+            return (LinkedHashMap) res;
+        }
+
+        Optional<Groups> optGroup = groupsRepository.findById(groupId);
+        Groups group = optGroup.get();
+
+        Optional<Users> optUser = usersRepository.findById(userId);
+        if(!optUser.isPresent()){
+            res.put("success", false);
+            res.put("reason", "Invalid user");
+            return (LinkedHashMap) res;
+        }
+        Users user = optUser.get();
+
+
+        User_Group membership = new User_Group(user, group, admin);
+        repository.save(membership);
+
+        res.put("success", true);
+        res.put("membership", membership);
+        return (LinkedHashMap) res;
+    }
+}
