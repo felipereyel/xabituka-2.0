938567cbb8e1626c422bc7b4f83ab9347ee4141d
vinicius <viniciusf.006@gmail.com>
2020-05-24 21:42:07 -0300

    feature: login screen base

---
+++ android_xabituca/Xabituca/app/src/main/java/com/example/xabituca/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.xabituca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+    public void redirectToMainActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
