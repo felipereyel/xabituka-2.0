b225163c26ebbbf4687eda83c248a4d782f31828
Gabriel <gabrielsousa9@gmail.com>
2020-05-24 22:24:07 -0400

    feature: add textview in login and signup screen

--- android_xabituca/Xabituca/app/src/main/java/com/example/xabituca/SignupActivity.java
+++ android_xabituca/Xabituca/app/src/main/java/com/example/xabituca/SignupActivity.java
@@ -6,21 +6,21 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 
-public class LoginActivity extends Activity {
+public class SignupActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
+        setContentView(R.layout.activity_signup);
     }
+
     public void redirectToMainActivity(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-    public void redirectToSignUpActivity(View view) {
-        Intent intent = new Intent(this, SignupActivity.class);
+    public void redirectToLoginActivity(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
 }
